d3c10bf466167b162cd97c08649656bee0fd9846
Agustín Arguedas <carguedas@pernixlabs.com>
2022-07-20 17:25:50 -0600

    Finished v1 Main API

---
+++ Controllers/CambiarContraController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using V_Vuelos_Main_API.Crypto;
+using V_Vuelos_Main_API.Models;
+
+namespace V_Vuelos_Main_API.Controllers
+{
+    public class CambiarContraController : ApiController
+    {
+        private V_Vuelos_Main_NotEncryptedEntities db;
+        Crypt c;
+        public CambiarContraController()
+        {
+            db = new V_Vuelos_Main_NotEncryptedEntities();
+            c = new Crypt();
+        }
+
+        // POST: api/CambiarContra
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult PutContrasenna(string id, Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != usuario.usuario1)
+            {
+                return BadRequest();
+            }
+
+            usuario.contrasena = c.encriptar(usuario.contrasena);
+
+            db.uspCambiarContrasenaUsuario(usuario.usuario1, usuario.contrasena);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (UsuarioExists(usuario.usuario1))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = usuario.usuario1 }, usuario);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UsuarioExists(string id)
+        {
+            return db.Usuario.Count(e => e.usuario1 == id) > 0;
+        }
+    }
+}
---
+++ Controllers/VuelosDescriptivosController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using V_Vuelos_Main_API.Crypto;
+using V_Vuelos_Main_API.Models;
+
+namespace V_Vuelos_Main_API.Controllers
+{
+    public class VuelosDescriptivosController : ApiController
+    {
+        private V_Vuelos_Main_NotEncryptedEntities db;
+        Crypt c;
+        public VuelosDescriptivosController()
+        {
+            db = new V_Vuelos_Main_NotEncryptedEntities();
+            c = new Crypt();
+        }
+
+        // GET: api/VuelosDescriptivos
+        public  List<uspRecuperarVuelos_Result> GetVuelo()
+        {
+            List<uspRecuperarVuelos_Result> resultado = db.uspRecuperarVuelos().ToList();
+
+            foreach (var vuelo in resultado)
+            {
+                vuelo.fecha_partida = c.desencriptar(vuelo.fecha_partida);
+                vuelo.hora_partida = c.desencriptar(vuelo.hora_partida);
+                vuelo.fecha_llegada = c.desencriptar(vuelo.fecha_llegada);
+                vuelo.hora_llegada = c.desencriptar(vuelo.hora_llegada);
+                vuelo.nombre_aerolinea = c.desencriptar(vuelo.nombre_aerolinea);
+                vuelo.numero_puerta = c.desencriptar(vuelo.numero_puerta);
+                vuelo.descripcion_estado = c.desencriptar(vuelo.descripcion_estado);
+                vuelo.nombre_pais_parte = c.desencriptar(vuelo.nombre_pais_parte);
+                vuelo.nombre_pais_llega = c.desencriptar(vuelo.nombre_pais_llega);
+            }
+
+            return resultado;
+        }
+    }
+}
